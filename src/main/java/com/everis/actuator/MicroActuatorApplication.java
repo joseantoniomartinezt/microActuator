f76524838bfc8b42feeb05f4ee0475117a790396
joseantonio <joseantoniomartinez1904@gmail.com>
2021-11-30 14:31:12 +0100

    first commit

---
+++ src/main/java/com/everis/actuator/MicroActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.everis.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroActuatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everis/actuator/MicroActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
